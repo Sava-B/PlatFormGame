7435d143a43824319f796535e35cb2716f8f7b7d
Eric Peterson <ericpeterson91@live.ca>
2022-09-24 13:37:46 -0600

    jumping

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaDog } from "react-icons/fa";
 import { IoAmericanFootballSharp } from "react-icons/io5";
 import "./GameBoard.css";
 
+const dogJump = () => {
+  console.log('clicked')
+  const dog = document.querySelector('.dog')
+  if(!dog.classList.contains('jump')) {
+    (dog.classList.add('jump'))
+  }
+  setTimeout(() => dog.classList.remove('jump'), 2000)
+}
+  
+
 const GameBoard = () => {
   return (
     <div className="container">
-      <div className="dog">
+      <div onClick={dogJump} className="dog">
         <FaDog size={60} />
       </div>
         

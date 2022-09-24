f00c4a305f860ca7f4bcee77d6586e69c40d49bc
Eric Peterson <ericpeterson91@live.ca>
2022-09-24 14:24:07 -0600

    background

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -4,8 +4,8 @@ import { IoAmericanFootballSharp } from "react-icons/io5";
 import Score from "./Score";
 import "./GameBoard.css";
 
-const dogJump = () => {
-  console.log("clicked");
+
+  const dogJump = () => {
   const dog = document.querySelector(".dog");
   if (!dog.classList.contains("jump")) {
     dog.classList.add("jump");

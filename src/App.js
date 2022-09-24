a115cb8f68ba5bb5ce217b0d16bc078b526b5bba
Eric Peterson <ericpeterson91@live.ca>
2022-09-24 12:53:08 -0600

    animation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react'
-import { FaDog } from 'react-icons/fa'
-import { IoAmericanFootballSharp } from 'react-icons/io5'
-
-
+import GameBoard from './components/GameBoard'
 
 const App = () => {
   return (
-    <div className='container'>
-      <FaDog size={60}/>
-      <div className='floor'>
-
-      </div>
-      <div className='obstacle'>
-        <IoAmericanFootballSharp size={60}/>
-      </div>
-    </div>
+    <GameBoard />
   )
 }
 

2ebd7aaa6238a9dbed77b5eb3ff6dc1f3edff853
Eric Peterson <ericpeterson91@live.ca>
2022-09-24 14:05:18 -0600

    bugs

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import { FaDog } from "react-icons/fa";
 import { IoAmericanFootballSharp } from "react-icons/io5";
-import Score from "./Score"
+import Score from "./Score";
 import "./GameBoard.css";
 
 const dogJump = () => {
-  console.log('clicked')
-  const dog = document.querySelector('.dog')
-  if(!dog.classList.contains('jump')) {
-    (dog.classList.add('jump'))
+  console.log("clicked");
+  const dog = document.querySelector(".dog");
+  if (!dog.classList.contains("jump")) {
+    dog.classList.add("jump");
   }
-  setTimeout(() => dog.classList.remove('jump'), 2000)
-}
-  
+  setTimeout(() => dog.classList.remove("jump"), 2000);
+};
 
 const GameBoard = () => {
   return (
     <div className="container">
-
       <div onClick={dogJump} className="dog">
-
-      <Score/>
-      <div className="dog">
-
         <FaDog size={60} />
       </div>
-        
+      <Score />
+
       <div className="obstacle">
         <IoAmericanFootballSharp size={60} />
       </div>

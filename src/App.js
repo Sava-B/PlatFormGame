474b21eb70188e8793cc7780ffdfe2de05eea9bb
andre996 <arrm.1996@gmail.com>
2022-09-24 13:37:18 -0600

    add: gitCommands update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
-import { FaDog } from 'react-icons/fa'
-import { IoAmericanFootballSharp } from 'react-icons/io5'
-import Score from './components/Score'
-
-
-
-const App = () => {
-  return (
-    <div className='container'>
-      <Score/>
-      <FaDog size={60}/>
-      <div className='floor'>
-
-      </div>
-      <div className='obstacle'>
-        <IoAmericanFootballSharp />
-      </div>
-    </div>
-=======
 import GameBoard from './components/GameBoard'
 
 const App = () => {
   return (
     <GameBoard />
->>>>>>> main
   )
 }
 

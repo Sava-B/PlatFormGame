dcda2d528157327496b180d55e42f23a16dead05
andre996 <arrm.1996@gmail.com>
2022-09-24 13:14:29 -0600

    feat: add score display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FaDog } from 'react-icons/fa'
 import { IoAmericanFootballSharp } from 'react-icons/io5'
+import Score from './components/Score'
 
 
 
 const App = () => {
   return (
     <div className='container'>
+      <Score/>
       <FaDog size={60}/>
       <div className='floor'>
 
